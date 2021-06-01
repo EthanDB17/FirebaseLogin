31efb9ece46e722f2706451d4d16b4e42f20b701
Ethan Borrowman <ethan_borrowman@hotmail.com>
2021-05-31 21:21:27 -0400

    Initial commit

---
+++ FirebaseLogin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirebaseLogin
+//
+//  Created by Ethan Borrowman on 5/31/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
